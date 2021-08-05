465b885aaf222f645eedc08403258041d7718a8a
vasile.costiuc <vasile.costiuc@it-lab.md>
2021-08-06 00:45:48 +0300

    Create migrations seeds

---
+++ database/seeders/AdminOrderProductsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminOrderProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [];
+        for($y = 1;$y<=12;$y++){
+            for($i=0;$i<=5;$i++){
+                $data[] = [
+                  'order_id' => $y,
+                  'product_id' => $i,
+                  'qty' => rand(1,4),
+                  'title' => "Casio test prod",
+                  'price' => rand(40,200),
+                ];
+            }
+
+        }
+        DB::table('order_products')->insert($data);
+    }
+}
